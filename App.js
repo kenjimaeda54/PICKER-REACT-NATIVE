ddf58658f44215f6e7e94528628953ed2f15fa39
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-03-16 23:29:01 -0300

    picker

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Picker } from "@react-native-picker/picker";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pizza: 0,
+      pizzas: [
+        { key: 1, nome: "Brigadeiro", valor: 50 },
+        { key: 2, nome: "Americana", valor: 20 },
+        { key: 3, nome: "Portuguesa", valor: 35 },
+        { key: 4, nome: "Caipiria", valor: 45 },
+      ],
+    };
+  }
+
+  render() {
+    let cardapio = this.state.pizzas.map((item, index) => {
+      return <Picker.item key={index} value={index} label={item.nome} />;
+      //em key e value são numeros e o index representa o numero ou posiçao
+    });
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>Menu Pizza</Text>
+        <Picker
+          selectedValue={this.state.pizza}
+          //onValueChange e o valor que ele recebe e vai alterar tem duas propriedades
+          //{caradpio} e para mostrar a piza selecionada.
+          //porque o correto é <Picker> <Picker.item> </Picer>
+          // e la em cima estamos retornando o Picker item
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ pizza: itemValue })
+          }
+        >
+          {cardapio}
+        </Picker>
+        <Text style={styles.pizzas}>
+          Voce escolheu: {this.state.pizzas[this.state.pizza].nome}
+        </Text>
+        <Text style={styles.pizzas}>
+          R$:{this.state.pizzas[this.state.pizza].valor.toFixed(2)}{" "}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+  },
+  logo: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  pizzas: {
+    marginTop: 15,
+    fontSize: 25,
+    textAlign: "center",
   },
 });
